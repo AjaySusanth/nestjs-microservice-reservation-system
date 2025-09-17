7f3d5f04c72d488deff6a5d9c8557c86f56275d5
AjaySusanth <ajaysusanth10@gmail.com>
2025-09-17 16:28:26 +0000

    added user roles

--- apps/auth/src/users/dto/create-user.dto.ts
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -1,8 +1,14 @@
-import { IsEmail, IsStrongPassword } from "class-validator"
+import { IsEmail, IsStrongPassword, IsOptional, IsArray, IsNotEmpty, IsString  } from "class-validator"
 
 export class CreateUserDto {
     @IsEmail()
     email:string
     @IsStrongPassword()
     password: string
+
+    @IsOptional()
+    @IsArray()
+    @IsNotEmpty({ each:true })
+    @IsString({ each:true })
+    roles?: string[]
 }
